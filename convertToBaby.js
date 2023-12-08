db39dcadabc4e0c57713ff6d45be01440042358c
Kyle Chung <85056257+itskylechung@users.noreply.github.com>
2023-12-08 23:50:10 +0800

    Create convertToBaby.js

---
+++ convertToBaby.js
@@ -0,0 +1,21 @@
+// Write your code here:
+const convertToBaby = arr =>{
+  const newArray = [];
+  for (let i = 0;i < arr.length ;i++){
+    newArray.push('baby '+ arr[i]);
+  }
+  return newArray;
+}
+
+
+
+
+
+
+
+// When you're ready to test your code, uncomment the below and run:
+/*
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+console.log(convertToBaby(animals)) 
+*/
