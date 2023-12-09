bf7d9d7bc52765704a3b86496c1714631be5e398
Kyle Chung <85056257+itskylechung@users.noreply.github.com>
2023-12-09 15:56:54 +0800

    Create declineEverything.js

---
+++ declineEverything.js
@@ -0,0 +1,17 @@
+
+const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+const politelyDecline = (veg) => {
+      console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+}
+
+// Write your code here:
+const declineEverything = (arr) => {
+  arr.forEach(politelyDecline);
+  return arr;
+}
+const acceptEverything = (arr) => {
+  arr.forEach(e => {
+    console.log(`Ok, I guess I will eat some ${e}.`)
+  })
+}
